d489e908755f5086defbb72734a97d22d3a8ffda
joppincal <106348367+joppincal@users.noreply.github.com>
2024-07-19 22:17:38 +0900

    Update __main__.py
    Add and organize comments, exception handling, and log output

--- tests/__main__.py
+++ tests/__main__.py
@@ -8,6 +8,7 @@ import shutil
 import subprocess
 import sys
 import tempfile
+import time
 import webbrowser
 import winreg
 from logging import (
@@ -24,16 +25,27 @@ from typing import Callable, Dict, List, Optional
 
 import click
 
-# Constants
+# Location of configuration and log files
 ROOT_DIR = Path.home() / ".browser_starter"
+# Location of configuration file
 CONFIG_FILE = ROOT_DIR / "browser_starter.json"
+# Time before the start page automatically closes
 DEFAULT_COUNTDOWN_SECONDS = 6
 
-# Registered browsers
+# Browser name and its path on the PC
 REGISTERED_BROWSERS: Dict[str, str] = {}
 
 
 def log_setting(fileout: bool = True, stdout: bool = False):
+    """Logging Settings
+
+    Args:
+        fileout (bool, optional): Log file output or not. Defaults to True.
+        stdout (bool, optional): Output std output or not. Defaults to False.
+
+    Returns:
+        _type_: logging.Logger
+    """
     logger = getLogger(__name__)
 
     if not stdout and not fileout:
@@ -78,14 +90,16 @@ def load_config() -> Dict:
     try:
         with open(CONFIG_FILE, "r") as f:
             return json.load(f)
+
     except FileNotFoundError:
         logger.warning(
             f"Config file {CONFIG_FILE} not found. Using default settings."
         )
-        return {}
+
     except json.JSONDecodeError:
         logger.error(f"Error decoding {CONFIG_FILE}. Using default settings.")
-        return {}
+
+    return {}
 
 
 CONFIG = load_config()
@@ -95,15 +109,22 @@ def get_browser_path_windows(browser_name: str) -> Optional[str]:
     """
     Get browser path from Windows registry.
     """
+    logger.debug(f"Attempting to get browser path for {browser_name}")
+
     try:
         browser_key = rf"Software\Clients\StartMenuInternet\{browser_name}"
         key_path = rf"{browser_key}\shell\open\command"
+
         with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path) as key:
             command = winreg.QueryValueEx(key, "")[0]
             path = command.replace('"', "")
+            logger.debug(f"Browser path found: {path}")
+
             return path
+
     except WindowsError as e:
         logger.error(f"Error getting browser path for {browser_name}: {e}")
+
         return None
 
 
@@ -112,15 +133,27 @@ def get_default_browser_path_windows() -> Optional[str]:
     Get default browser path from Windows registry.
     """
     try:
+        logger.debug("Attempting to get default browser path")
+
         key_path = r"Software\Microsoft\Windows\Shell"
         key_path += r"\Associations\UrlAssociations\https\UserChoice"
+
+        # Get the ProgID from the user settings that open the Https link.
         with winreg.OpenKey(winreg.HKEY_CURRENT_USER, key_path) as key:
             progid = winreg.QueryValueEx(key, "ProgID")[0]
+        logger.debug(f"Default browser ProgID: {progid}")
 
         key_path = rf"{progid}\shell\open\command"
+
+        # Get software path from ProgID
         with winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, key_path) as key:
             command = winreg.QueryValueEx(key, "")[0]
             match = re.search(r'"?([^"]+\.exe)"?', command)
+            logger.debug(
+                f"Default browser path found: "
+                f"{match.group(1) if match else None}"
+            )
+
             if match:
                 return match.group(1)
             else:
@@ -128,6 +161,7 @@ def get_default_browser_path_windows() -> Optional[str]:
 
     except WindowsError as e:
         logger.error(f"Error getting default browser path: {e}")
+
         return None
 
 
@@ -136,9 +170,11 @@ def get_installed_browsers() -> Dict[str, Optional[str]]:
     Get installed browsers based on the operating system.
     """
     system = platform.system()
+    logger.info(f"Detecting installed browsers on {system}")
+
+    browsers: Dict[str, Optional[str]] = {}
 
     if system == "Windows":
-        browsers: Dict[str, Optional[str]] = {}
         keys = [
             r"SOFTWARE\Clients\StartMenuInternet",
             r"SOFTWARE\WOW6432Node\Clients\StartMenuInternet",
@@ -149,11 +185,13 @@ def get_installed_browsers() -> Dict[str, Optional[str]]:
                     for i in range(winreg.QueryInfoKey(reg_key)[0]):
                         name = winreg.EnumKey(reg_key, i)
                         browsers[name] = get_browser_path_windows(name)
+
             except WindowsError as e:
                 logger.error(f"Error accessing registry key {key}: {e}")
-        return dict(sorted(browsers.items()))
 
-    if system == "Linux":
+        browsers = dict(sorted(browsers.items()))
+
+    elif system == "Linux":
         common_browsers = [
             "firefox",
             "google-chrome",
@@ -163,28 +201,34 @@ def get_installed_browsers() -> Dict[str, Optional[str]]:
             "vivaldi",
         ]
 
-        return {
+        browsers = {
             browser: shutil.which(browser)
             for browser in common_browsers
             if shutil.which(browser)
         }
 
-    if system == "Darwin":  # macOS
+    elif system == "Darwin":  # macOS
         # TODO: Implement macOS browser detection here
         logger.warning("macOS browser detection not implemented yet.")
-        return {}
 
-    logger.warning(f"Unsupported operating system: {system}")
-    return {}
+    else:
+        logger.warning(f"Unsupported operating system: {system}")
+
+    logger.debug(f"Detected browsers: {browsers}")
+
+    return browsers
 
 
 def register_browser(name: str, path: str) -> None:
     """
     Register a browser.
     """
+    logger.info(f"Registering browser: {name} at path: {path}")
+
     try:
         webbrowser.register(name, None, webbrowser.BackgroundBrowser(path))
         REGISTERED_BROWSERS[name] = path
+
     except Exception as e:
         logger.error(f"Error registering browser {name}: {e}")
 
@@ -204,6 +248,7 @@ async def open_url(browser: webbrowser.BaseBrowser, url: str) -> None:
     """
     success = browser.open(url, new=2)  # new=2 specifies opening in a new tab
     logger.info(f"Opening URL: {url} - Success: {success}")
+
     await asyncio.sleep(0.5)
 
 
@@ -215,6 +260,7 @@ async def open_urls_in_browser(
     """
     browser = webbrowser.get(browsername)
     browserpath = REGISTERED_BROWSERS[browsername]
+    logger.debug(f"Browser path: {browserpath}")
 
     logger.info(f"Opening URLs in {browsername}")
     logger.debug(f"Registered browsers: {REGISTERED_BROWSERS}")
@@ -223,6 +269,8 @@ async def open_urls_in_browser(
     )
 
     start_page = get_start_page()
+    logger.debug(f"Start page URL: {start_page}")
+
     process = subprocess.Popen(
         [browserpath, "--new-window", start_page], shell=True  # type: ignore
     )
@@ -297,16 +345,21 @@ def get_start_page(
         ) as f:
             f.write(html_content)
             temp_file_name = f.name
+        logger.debug(f"Created temporary start page: {temp_file_name}")
 
         atexit.register(lambda: os.remove(temp_file_name))
 
         return Path(temp_file_name).absolute().as_uri()
     except IOError as e:
         logger.error(f"Error creating temporary file: {e}")
+
         return None
 
 
 def display_registered_browsers():
+    """
+    Registered Name |> Path
+    """
     if not REGISTERED_BROWSERS:
         click.echo("No browsers registered.")
         return
@@ -340,13 +393,26 @@ async def main(
     """
     Main asynchronous function to open URLs in specified browsers.
     """
+    start_time = time.perf_counter()
+
     register_all_installed_browsers()
     open_strategy = open_urls_fast if fast_mode else open_urls_ordered
     tasks = [
         open_urls_in_browser(browsername, urls, open_strategy)
         for browsername in browsernames
     ]
-    await asyncio.gather(*tasks)
+    logger.info(f"Starting main function with browsers: {browsernames}")
+    logger.info(f"URLs to open: {urls}")
+    logger.info(f"Fast mode: {fast_mode}")
+
+    try:
+        await asyncio.gather(*tasks)
+    except asyncio.CancelledError as e:
+        logger.warning("Main function cancelled:", exc_info=e)
+    finally:
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+        logger.info(f"Total execution time: {elapsed_time:.4f} seconds")
 
 
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
@@ -391,14 +457,20 @@ async def main(
 def cli(browser_name, browser_path, config, fast, browser_list, urls, urls_):
     """Open URLs in specified browser.
     If no URLs are provided, only the start page will be opened."""
+    logger.info("Starting CLI")
+    logger.debug(f"Command line arguments: {sys.argv}")
+
+    # Display help when no argument is given
     if len(sys.argv) == 1:
         cli.main(["--help"])
 
+    # Display browser list and exit
     if browser_list:
         register_all_installed_browsers()
         display_registered_browsers()
         return
 
+    # When a parameter file is passed, process accordingly
     if config:
         if browser_name or browser_path:
             click.echo(
@@ -411,6 +483,7 @@ def cli(browser_name, browser_path, config, fast, browser_list, urls, urls_):
         return
 
     browsers = list()
+    # If neither bn nor bp is specified, get the user default for the computer
     if not any([browser_name, browser_path]):
         default_browser_path = get_default_browser_path_windows()
         if default_browser_path:
@@ -418,14 +491,19 @@ def cli(browser_name, browser_path, config, fast, browser_list, urls, urls_):
             browsers.append(default_browser_path)
         else:
             pass
+    # When bp is specified, set to webbrowser
     if browser_path:
         for name in browser_path:
             register_browser(name, name)
         browsers.extend(list(browser_path))
+    # When bn is specified, check if webbrowser is configured
     if browser_name:
         browsers.extend(list(browser_name))
 
+    logger.info(f"Selected browsers: {browsers}")
+
     if urls and urls_:
+        # Because the firing order of URLs can no longer be guaranteed
         click.echo(
             "Warning: Do not mix option names --urls with and without "
             "omitted ones. The omitted notation is recommended."
@@ -433,6 +511,8 @@ def cli(browser_name, browser_path, config, fast, browser_list, urls, urls_):
         return
     if not urls:
         urls = urls_
+
+    logger.info(f"URLs to open: {urls}")
 
     asyncio.run(main(browsers, urls, fast))
 
